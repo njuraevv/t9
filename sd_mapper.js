654ee534d424d840d91be1697fda10263ad50e36
Nosirjon Juraev <34993829+njuraevv@users.noreply.github.com>
2021-02-22 22:07:43 -0600

    Add files via upload

---
+++ sd_mapper.js
@@ -0,0 +1,35 @@
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Case Problem 1
+
+   Planisphere Script
+   Author: 
+   Date:   
+
+*/
+
+"use strict";
+//first try this:
+//var thisTime = new Date("February 3, 2018 03:15:28 AM");
+
+//after that, change the date to current date:
+var thisTime = new Date("February 22, 2021 10:03:00 PM")
+var timeStr = thisTime.toLocaleString();
+
+
+document.getElementById("timeStamp").innerHTML = timeStr;
+
+
+var thisHour = thisTime.getHours();
+var thisMonth = thisTime.getMonth();
+
+
+var mapNum = ((2*thisMonth + thisHour)%24);
+
+
+var imgStr = "<img src='sd_sky" + mapNum + ".png'>"; 
+
+
+document.getElementById("planisphere").insertAdjacentHTML("afterBegin", imgStr);
+
